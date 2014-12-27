1631b29ee730364ef8f16ea881791810493d84ae
Ji Ho Koo <jihokoo@gmail.com>
2014-12-27 23:35:17 +0900

    add hash function for user password

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+  "golang.org/x/crypto/bcrypt"
+  "log"
+)
+
 type User struct {
   // TODO: make the id a universally unique id
   Id int64
@@ -8,5 +13,15 @@ type User struct {
   UserName string
   FirstName string
   LastName string
+  Password []byte
   // TODO: hash password
+}
+
+func (u *User) HashPassword(password string) {
+  hashedPassword, hashError := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+  if hashError != nil {
+      log.Fatal(hashError)
+      panic(hashError) //this is a panic because bcrypt errors on invalid costs
+  }
+  u.Password = hashedPassword
 }
